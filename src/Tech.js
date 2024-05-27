27bc576014e001fa627d4e030f822165bbcac827
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-05-27 15:18:05 +0300

    update tech

--- src/Tech.js
+++ src/Tech.js
@@ -1,88 +1,49 @@
 import React from "react";
+import Marquee from "react-marquee-slider";
 import html from "./images/2284975.webp";
 import css from "./images/css-3.svg";
 import js from "./images/js-logo-png-5.webp";
 import tailwindcss from "./images/tailwind-css-logo-5AD4175897-seeklogo.com.webp";
 import bootstrapIcone from "./images/bootstrap-5-logo-icon.svg";
+import typescript from "./images/TypeScript-logo.webp";
 import laravel from "./images/laravel-1.svg";
 import reactIcone from "./images/react-2.svg";
 import nodejs from "./images/nodejs-new-pantone-white.svg";
+import express from "./images/Expressjs-nodejs.webp";
+import php from "./images/58481791cef1014c0b5e4994.webp";
 
 function Tech() {
+  const techImages = [
+    { src: html, label: "HTML" },
+    { src: css, label: "CSS" },
+    { src: js, label: "JavaScript" },
+    { src: tailwindcss, label: "Tailwind" },
+    { src: bootstrapIcone, label: "Bootstrap" },
+    { src: typescript, label: "TypeScript" },
+    { src: reactIcone, label: "React" },
+    { src: php, label: "PHP" },
+    { src: laravel, label: "Laravel" },
+    { src: nodejs, label: "Node.js" },
+    { src: express, label: "Express.js" },
+  ];
+
   return (
     <div className="mb-20">
       <div className="flex justify-center mt-32">
-        <h1 className="nom m-5 text-3xl  font-medium  border-b border-pink-500">
+        <h1 className="nom m-5 text-3xl font-medium border-b border-pink-500">
           Tech
         </h1>
       </div>
-      <div className="flex justify-around mt-5">
-        <div className="tech">
-          <img src={html} alt="" style={{ height: "30px" }} className="ms-8" />
-          <br />
-          <p className="badge">HTML</p>
-        </div>
-        <div className="tech">
-          <img src={css} alt="" style={{ height: "30px" }} className="ms-6" />
-          <br />
-          <p className="badge">CSS</p>
-        </div>
-        <div className="tech">
-          <img src={js} alt="" style={{ height: "30px" }} className="ms-14" />
-          <br />
-          <p className="badge">JAVASCRIPT</p>
-        </div>
-        <div className="tech">
+      <Marquee gradient={false} speed={60} cycle={0} resetAfterTries={200}>
+        {techImages.map((tech, index) => (
           <img
-            src={tailwindcss}
-            alt=""
-            style={{ height: "30px" }}
-            className="ms-10"
+            key={`tech-${index}`}
+            src={tech.src}
+            alt={tech.label}
+            style={{ height: "40px", marginRight: "50px" }}
           />
-          <br />
-          <p className="badge">TAILWIND</p>
-        </div>
-        <div className="tech">
-          <img
-            src={bootstrapIcone}
-            alt=""
-            style={{ height: "30px" }}
-            className="ms-12"
-          />
-          <br />
-          <p className="badge">BOOTSTRAP</p>
-        </div>
-        <div className="tech">
-          <img
-            src={reactIcone}
-            alt=""
-            style={{ height: "30px" }}
-            className="ms-9"
-          />
-          <br />
-          <p className="badge">REACT</p>
-        </div>
-        <div className="tech">
-          <img
-            src={laravel}
-            alt=""
-            style={{ height: "30px" }}
-            className="ms-8"
-          />
-          <br />
-          <p className="badge">LARAVEL</p>
-        </div>
-        <div className="tech">
-          <img
-            src={nodejs}
-            alt=""
-            style={{ height: "30px" }}
-            className="ms-7"
-          />
-          <br />
-          <p className="badge">NODEJS</p>
-        </div>
-      </div>
+        ))}
+      </Marquee>
     </div>
   );
 }
